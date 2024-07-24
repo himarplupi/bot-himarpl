47b0662b52cf70a115abffbf58315816cff232e2
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-07-24 11:19:50 +0700

    feat: init telegram bot api first connection

---
+++ api/telegram/webhook.ts
@@ -0,0 +1,67 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = process.env.TELEGRAM_TOKEN;
+
+if (!TOKEN) {
+  throw new Error("TELEGRAM_TOKEN is required");
+}
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`https://bot.himarpl.com/api/telegram/webhook`);
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  // @ts-ignore
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "Received your message");
+});
+
+function GET(req: VercelRequest, res: VercelResponse) {
+  bot.processUpdate(req.body);
+
+  return res.json({
+    ok: true,
+    description: "Success",
+    result: `Webhook processed`,
+  });
+}
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    switch (req.method) {
+      case "GET":
+        return GET(req, res);
+      default:
+        return res.status(405).json({
+          ok: false,
+          description: "Method Not Allowed",
+        });
+    }
+  } catch (e) {
+    return res.status(500).json({
+      ok: false,
+      description: "Internal Server Error",
+    });
+  }
+}
