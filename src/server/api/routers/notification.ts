599165b653911888f3919d606e1ebda1c165c2ba
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-07-26 11:49:19 +0700

    feat(server): add trpc router for notify

--- src/server/api/routers/notification.ts
+++ src/server/api/routers/notification.ts
@@ -1,4 +1,7 @@
+import { arrayContains, eq, not } from "drizzle-orm";
 import { z } from "zod";
+
+import { TRPCError } from "@trpc/server";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { notifications } from "~/server/db/schema";
@@ -14,7 +17,7 @@ export const notificationRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(notifications).values({
+      return await ctx.db.insert(notifications).values({
         chatId: input.chatId,
         firstName: input.firstName,
         lastName: input.lastName,
@@ -22,6 +25,65 @@ export const notificationRouter = createTRPCRouter({
       });
     }),
   getMany: publicProcedure.query(async ({ ctx }) => {
-    return ctx.db.select().from(notifications);
+    return await ctx.db.select().from(notifications);
   }),
+  getUnnotify: publicProcedure
+    .input(z.object({ slug: z.string(), limit: z.number().default(25) }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.query.notifications.findMany({
+        limit: input.limit,
+        where: (notification) =>
+          not(arrayContains(notification.notifying, [input.slug])),
+      });
+    }),
+  notify: publicProcedure
+    .input(z.object({ slug: z.string(), chatId: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      const prev = await ctx.db.query.notifications.findFirst({
+        where: (notification) => eq(notification.chatId, input.chatId),
+        columns: {
+          notifying: true,
+        },
+      });
+
+      if (!prev) {
+        return new TRPCError({
+          code: "NOT_FOUND",
+          message: "Notification not found",
+        });
+      }
+
+      return await ctx.db
+        .update(notifications)
+        .set({
+          notifying: [...prev.notifying, input.slug],
+        })
+        .where(eq(notifications.chatId, input.chatId));
+    }),
+  removeNotify: publicProcedure
+    .input(z.object({ slug: z.string(), chatId: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      const prev = await ctx.db.query.notifications.findFirst({
+        where: (notification) => eq(notification.chatId, input.chatId),
+        columns: {
+          notifying: true,
+        },
+      });
+
+      if (!prev) {
+        return new TRPCError({
+          code: "NOT_FOUND",
+          message: "Notification not found",
+        });
+      }
+
+      const newNotifying = prev.notifying.filter((slug) => slug !== input.slug);
+
+      return await ctx.db
+        .update(notifications)
+        .set({
+          notifying: newNotifying,
+        })
+        .where(eq(notifications.chatId, input.chatId));
+    }),
 });
