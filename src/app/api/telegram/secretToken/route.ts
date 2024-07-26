a7ad7a312afca912fe0a529e6cd9157fe923391e
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-07-26 14:07:33 +0700

    feat(secretToken): add ratelimit in secretToken route

--- src/app/api/telegram/secretToken/route.ts
+++ src/app/api/telegram/secretToken/route.ts
@@ -1,10 +1,31 @@
 import { NextResponse } from "next/server";
 
+import { ipAddress } from "@vercel/functions";
+
 import { generateSecretToken } from "~/lib/hash";
+import { ratelimit } from "~/server/ratelimit";
 
 export const runtime = "edge";
 
 export async function GET(request: Request) {
+  // Rate limit the request
+  const ip = ipAddress(request) ?? "unknown";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) {
+    return NextResponse.json(
+      {
+        ok: false,
+        description: "Too Many Requests",
+        result: "Rate limit exceeded",
+      },
+      {
+        status: 429,
+        statusText: "Too Many Requests",
+      },
+    );
+  }
+
   const token = request.headers.get("x-telegram-bot-token");
   const username = request.headers.get("x-telegram-bot-username");
 
